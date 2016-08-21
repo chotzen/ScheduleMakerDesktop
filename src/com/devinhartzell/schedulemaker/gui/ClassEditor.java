90a591d385b08513c18bcde024cd3edd51e4f2a0
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2016-08-20 23:35:47 -1000

    Add a color picker, among other things

--- src/com/devinhartzell/schedulemaker/gui/ClassEditor.java
+++ src/com/devinhartzell/schedulemaker/gui/ClassEditor.java
@@ -2,15 +2,36 @@ package com.devinhartzell.schedulemaker.gui;
 
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
-import java.awt.Font;
-import javax.swing.JSpinner;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JEditorPane;
+import javax.swing.JRadioButton;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JSpinner;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.SpinnerNumberModel;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 
 public class ClassEditor extends JFrame {
 
 	private static final long serialVersionUID = 8426220918159293409L;
 	private JComboBox<String> startTimeMenu;
+	private JComboBox<String> endTimeMenu;
+	private JEditorPane className;
+	private JEditorPane location;
+	private JRadioButton[] daySelectors = new JRadioButton[6];
+	
+	private String[] letters = {"A", "B", "C", "D", "E", "F"};
+	private JSpinner red;
+	private JSpinner green;
+	private JSpinner blue;
 	
 	public ClassEditor(int timeSlotID, int defaultDay) {
 		super("Class Editor");
@@ -19,9 +40,95 @@ public class ClassEditor extends JFrame {
 		startTimeMenu = new JComboBox<String>();
 		startTimeMenu.setModel(new DefaultComboBoxModel<String>(new String[] {"7:30", "7:45", "8:00", "8:15", "8:30", "8:45", "9:00", "9:15", "9:30", "9:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15"}));
 		startTimeMenu.setSelectedIndex(timeSlotID);
-		startTimeMenu.setBounds(80, 11, 66, 20);
+		startTimeMenu.setBounds(114, 11, 66, 20);
 		getContentPane().add(startTimeMenu);
-		setSize(450, 300);
+		
+		endTimeMenu = new JComboBox<String>();
+		endTimeMenu.setModel(new DefaultComboBoxModel<String>(new String[] {"7:40", "7:55", "8:10", "8:25", "8:40", "8:55", "9:10", "9:25", "9:40", "9:55", "10:10", "10:25", "10:40", "10:55", "11:10", "11:25", "11:40", "11:55", "12:10", "12:25", "12:40", "12:55", "13:10", "13:25", "13:40", "13:55", "14:10", "14:25", "14:40", "14:55", "15:10", "15:25"}));
+		endTimeMenu.setSelectedIndex(timeSlotID + 3);
+		endTimeMenu.setBounds(114, 42, 66, 20);
+		getContentPane().add(endTimeMenu);
+		
+		JTextArea txtrStartTime = new JTextArea();
+		txtrStartTime.setEditable(false);
+		txtrStartTime.setText("Start Time");
+		txtrStartTime.setBounds(10, 9, 94, 22);
+		getContentPane().add(txtrStartTime);
+		
+		JTextArea txtrEndTime = new JTextArea();
+		txtrEndTime.setEditable(false);
+		txtrEndTime.setText("End Time");
+		txtrEndTime.setBounds(10, 42, 94, 22);
+		getContentPane().add(txtrEndTime);
+		
+		className = new JEditorPane();
+		className.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				className.setText("");
+			}
+		});
+		className.setText("Class Name");
+		className.setBounds(190, 11, 121, 20);
+		getContentPane().add(className);
+		
+		location = new JEditorPane();
+		location.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				location.setText("");
+			}
+		});
+		location.setText("Location");
+		location.setBounds(321, 11, 53, 20);
+		getContentPane().add(location);
+		
+		JPanel colordemo = new JPanel();
+		colordemo.setBackground(new Color(0,0,0));
+		colordemo.setBounds(10, 73, 55, 20);
+		getContentPane().add(colordemo);
+		
+		
+		red = new JSpinner();
+		red.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				colordemo.setBackground(new Color((Integer)red.getValue(), (Integer)green.getValue(), (Integer)blue.getValue()));
+			}
+		});
+
+		red.setModel(new SpinnerNumberModel(0, 0, 255, 1));
+		red.setBounds(75, 73, 47, 20);
+		getContentPane().add(red);
+		
+		green = new JSpinner();
+		green.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				colordemo.setBackground(new Color((Integer)red.getValue(), (Integer)green.getValue(), (Integer)blue.getValue()));
+			}
+		});
+		green.setModel(new SpinnerNumberModel(0, 0, 255, 1));
+		green.setBounds(124, 73, 47, 20);
+		getContentPane().add(green);
+		
+		blue = new JSpinner();
+		blue.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				colordemo.setBackground(new Color((Integer)red.getValue(), (Integer)green.getValue(), (Integer)blue.getValue()));
+			}
+		});
+		blue.setModel(new SpinnerNumberModel(0, 0, 255, 1));
+		blue.setBounds(174, 73, 47, 20);
+		getContentPane().add(blue);
+		
+		
+		for (int i = 0; i <= 5; i++) {
+			daySelectors[i] = new JRadioButton(letters[i]);
+			daySelectors[i].setBounds(190 + (34 * i), 42, 35, 23);
+			if (i == defaultDay) daySelectors[i].setSelected(true);
+			getContentPane().add(daySelectors[i]);
+		}
+		
+		setSize(420, 300);
 		setVisible(true);
 		
 	}
